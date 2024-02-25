ede638d0e739b6d9b95a807c27bce391fb4d5dbd
Virginia Mesa <virginia.mesa@auctane.com>
2024-02-25 20:45:26 +0100

    tests react router

--- testing-heroes/src/heroes/pages/SearchPage.jsx
+++ testing-heroes/src/heroes/pages/SearchPage.jsx
@@ -41,7 +41,7 @@ export const SearchPage = () => {
           <div className="col-5">
             <h4>Searching</h4>
             <hr />
-            <form onSubmit={ onSearchSubmit }>
+            <form onSubmit={ onSearchSubmit } aria-label='form' >
               <input 
                 type="text"
                 placeholder="Search a hero"
@@ -74,7 +74,7 @@ export const SearchPage = () => {
               Search a hero
             </div>
 
-            <div className="alert alert-danger animate__animated animate__fadeIn" 
+            <div aria-label="alert-no-hero" className="alert alert-danger animate__animated animate__fadeIn" 
                 style={{ display: showError ? '' : 'none' }}>
               No hero with <b>{ q }</b>
             </div>
--- testing-heroes/tests/heroes/pages/SearchPage.test.jsx
+++ testing-heroes/tests/heroes/pages/SearchPage.test.jsx
@@ -1,8 +1,16 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, renderHook, screen } from "@testing-library/react"
 import { SearchPage } from "../../../src/heroes/pages/SearchPage"
-import { MemoryRouter } from "react-router-dom"
+import { MemoryRouter, useNavigate } from "react-router-dom"
+
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+  }))
 
 describe('Tests in <SearchPage />', () => { 
+ 
+    beforeEach( () => { jest.clearAllMocks() } )
     
     test('should render the component', () => { 
         
@@ -25,14 +33,52 @@ describe('Tests in <SearchPage />', () => {
             </MemoryRouter>
         )   
 
-        screen.debug()
         const input = screen.getByRole('textbox', { value: { name: 'searchText' } })
         expect( input.value ).toBe('batman')
 
         const headingTitle = screen.getByRole('heading', { level: 5 })   
-        expect( headingTitle ).toContain('Batman')
-        
+        expect( headingTitle.textContent ).toBe('Batman')
 
+        const divNoHero = screen.getByLabelText('alert-no-hero')
+        expect( divNoHero.style.display ).toBe('none')
 
       })
+
+      test('should show an error if it doesnt find the hero (batman123)', () => { 
+        
+        render( 
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <SearchPage/> 
+            </MemoryRouter>
+        )  
+
+        const divNoHero = screen.getByLabelText('alert-no-hero')
+        expect( divNoHero.style.display ).toBe('')
+        expect( divNoHero.innerHTML ).toContain('batman123')
+
+       })
+
+       test('should call the navigate method to see a hero', () => { 
+
+        const navigate = jest.fn()
+        useNavigate.mockReturnValue(navigate)
+        
+        render( 
+            <MemoryRouter>
+                <SearchPage/> 
+            </MemoryRouter>
+        )   
+
+        screen.debug()
+
+        const formInput = screen.getByRole('textbox')
+        fireEvent.change( formInput, { target: { name: 'searchText', value: 'superman' }})
+
+        const form = screen.getByLabelText('form')
+        fireEvent.submit( form )
+
+        expect(navigate).toHaveBeenCalledWith('?q=superman')
+
+
+        })
  })
